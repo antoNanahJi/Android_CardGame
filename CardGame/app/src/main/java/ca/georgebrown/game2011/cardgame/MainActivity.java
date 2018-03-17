0c79eed4e60c3e1dbd19d2d6853c0e3e2c4e0e2f
antoNanahJi <Anto.NanahJi@georgebrown.ca>
2018-03-17 11:50:07 -0400

    Round 4

--- CardGame/app/src/main/java/ca/georgebrown/game2011/cardgame/MainActivity.java
+++ CardGame/app/src/main/java/ca/georgebrown/game2011/cardgame/MainActivity.java
@@ -1,8 +1,10 @@
 package ca.georgebrown.game2011.cardgame;
 
+import android.content.DialogInterface;
 import android.media.Image;
 import android.os.CountDownTimer;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -45,9 +47,7 @@ public class MainActivity extends AppCompatActivity {
     ImageButton id1;
     ImageButton id2;
 
-    Button restartBtn;
-
-    TextView TextV;
+    TextView textV;
     TextView scoreText;
     TextView timerText;
     int backImage;
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextV=(TextView) findViewById(R.id.textView);
+        textV=(TextView) findViewById(R.id.textView);
         scoreText=(TextView) findViewById(R.id.textscore);
         timerText=(TextView) findViewById(R.id.texttimer) ;
 
@@ -91,7 +91,6 @@ public class MainActivity extends AppCompatActivity {
         backImage = R.drawable.cardback;
 
         scoreText.setText(String.valueOf(Score));
-        restartBtn=(Button) findViewById(R.id.ReStart);
 
         new CountDownTimer(30000, 1000) {
 
@@ -101,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
 
             public void onFinish() {
                 timerText.setText("Game Over!!!");
+                restartGame();
             }
         }.start();
         for(int i=0;i<16;i+=2)
@@ -120,47 +120,22 @@ public class MainActivity extends AppCompatActivity {
             outCome[rand2]=r;
 
         }
-        restartBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Count=0;
-                Score=0;
-                scoreText.setText("Score" + String.valueOf(Score));
-                for(int i=0;i<16;i++)
-                {
-                    imageButtons[i].setVisibility(View.VISIBLE);
-                    imageButtons[i].setImageResource(backImage);
-                }
-                for(int i=0;i<16;i+=2)
-                {
-                    int r=(int) Math.floor((Math.random()*52));//Generating nrand number
-                    outCome[i]=r;
-                    outCome[i+1]=r;
-                }
-                for(int i=0;i<16;i++)
-                {
-                    rand1 = (int) Math.floor((Math.random() * 8)); //Generating nrand number
-
-                    rand2 = (int) Math.floor((Math.random() * 8) + 8);//Generating nrand number
-
-                    int r=outCome[rand1];
-                    outCome[rand1]=outCome[rand2];
-                    outCome[rand2]=r;
-
-                }
-            }
-        });
-
-
 
         image1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Count++;
+                    textV.setText("Now pick another card...");
                     if (Count <= 2) {
                         image1.setImageResource(myImages[outCome[0]]);
                         image1.setTag(String.valueOf(myImages[outCome[0]]));
-                        checkID(image1, Count);
+                        new android.os.Handler().postDelayed(
+                                new Runnable() {
+                                    public void run() {
+                                        checkID(image1, Count);
+                                    }
+                                },
+                                500);
                     } else if (Count > 2) {
                         Count = 1;
                     }
@@ -170,11 +145,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image2.setImageResource(myImages[outCome[1]]);
                     image2.setTag(String.valueOf(myImages[outCome[1]]));
-                    checkID(image2, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image2, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -186,11 +168,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image3.setImageResource(myImages[outCome[2]]);
                     image3.setTag(String.valueOf(myImages[outCome[2]]));
-                    checkID(image3, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image3, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -203,11 +192,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image4.setImageResource(myImages[outCome[3]]);
                     image4.setTag(String.valueOf(myImages[outCome[3]]));
-                    checkID(image4, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image4, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -220,11 +216,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image5.setImageResource(myImages[outCome[4]]);
                     image5.setTag(String.valueOf(myImages[outCome[4]]));
-                    checkID(image5, Count);
+                    checkID(image5, Count);new android.os.Handler().postDelayed(
+                        new Runnable() {
+                            public void run() {
+                                checkID(image5, Count);
+                            }
+                        },
+                        500);
                 }
                 else if(Count >2)
                 {
@@ -236,11 +239,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image6.setImageResource(myImages[outCome[5]]);
                     image6.setTag(String.valueOf(myImages[outCome[5]]));
-                    checkID(image6, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image6, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -252,11 +262,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image7.setImageResource(myImages[outCome[6]]);
                     image7.setTag(String.valueOf(myImages[outCome[6]]));
-                    checkID(image7, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image7, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -268,11 +285,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image8.setImageResource(myImages[outCome[7]]);
                     image8.setTag(String.valueOf(myImages[outCome[7]]));
-                    checkID(image8, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image8, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -284,11 +308,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image9.setImageResource(myImages[outCome[8]]);
                     image9.setTag(String.valueOf(myImages[outCome[8]]));
-                    checkID(image9, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image9, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -300,11 +331,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image10.setImageResource(myImages[outCome[9]]);
                     image10.setTag(String.valueOf(myImages[outCome[9]]));
-                    checkID(image10, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image10, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -316,11 +354,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image11.setImageResource(myImages[outCome[10]]);
                     image11.setTag(String.valueOf(myImages[outCome[10]]));
-                    checkID(image11, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image11, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -332,11 +377,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image12.setImageResource(myImages[outCome[11]]);
                     image12.setTag(String.valueOf(myImages[outCome[11]]));
-                    checkID(image12, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image12, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -348,11 +400,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image13.setImageResource(myImages[outCome[12]]);
                     image13.setTag(String.valueOf(myImages[outCome[12]]));
-                    checkID(image13, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image13, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -364,11 +423,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image14.setImageResource(myImages[outCome[13]]);
                     image14.setTag(String.valueOf(myImages[outCome[13]]));
-                    checkID(image14, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image14, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -380,11 +446,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image15.setImageResource(myImages[outCome[14]]);
                     image15.setTag(String.valueOf(myImages[outCome[14]]));
-                    checkID(image15, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image15, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -396,11 +469,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Count++;
+                textV.setText("Now pick another card...");
                 if(Count<=2)
                 {
                     image16.setImageResource(myImages[outCome[15]]);
                     image16.setTag(String.valueOf(myImages[outCome[15]]));
-                    checkID(image16, Count);
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    checkID(image16, Count);
+                                }
+                            },
+                            500);
                 }
                 else if(Count >2)
                 {
@@ -433,12 +513,14 @@ public class MainActivity extends AppCompatActivity {
                     Score++;
                     scoreText.setText("Score" + String.valueOf(Score));
                     Count = 0;
+                    textV.setText("Right!! Pick a card...");
                     id1.setVisibility(View.INVISIBLE);
                     id2.setVisibility(View.INVISIBLE);
                 }
                 else
                 {
                     Count = 0;
+                    textV.setText("Wrong! Pick again...");
                     id1.setImageResource(backImage);
                     id2.setImageResource(backImage);
                 }
@@ -449,10 +531,26 @@ public class MainActivity extends AppCompatActivity {
             }
             else
             {
-                Count=1;
-                id2=null;
-                id_2=null;
+                Count=0;
+                id1.setImageResource(backImage);
+                id2.setImageResource(backImage);
+                id1 = null;
+                id2 = null;
+                id_1 = null;
+                id_2 = null;
             }
         }
+    }
+    void restartGame()
+    {
+        AlertDialog.Builder restart= new AlertDialog.Builder(this);
+        restart.setTitle("You Lose!!!");
+        restart.setPositiveButton("Play Again?", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                recreate();
+            }
+        });
+        restart.show();
     }
 }
